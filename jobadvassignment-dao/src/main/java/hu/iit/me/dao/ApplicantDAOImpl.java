0402bc2d191501210139bb1c5f91486efc146f9f
Balazs Bordas <balazs.bord@gmail.com>
2018-12-11 16:03:01 +0100

    Removed unnecessary beans, imports. Set Spring logging level to info.

--- jobadvassignment-dao/src/main/java/hu/iit/me/dao/ApplicantDAOImpl.java
+++ jobadvassignment-dao/src/main/java/hu/iit/me/dao/ApplicantDAOImpl.java
@@ -1,22 +1,7 @@
 package hu.iit.me.dao;
 
 import hu.iit.me.model.Applicant;
-import hu.iit.me.util.filter.Filter;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-import java.util.List;
-
-import static com.google.common.collect.Lists.newArrayList;
 
 @Repository("applicantDAO")
 public class ApplicantDAOImpl extends AbstractDAOImpl<Applicant> implements ApplicantDAO {
--- jobadvassignment-web/src/main/java/web/config/JobAdvertisementApplicationContext.java
+++ jobadvassignment-web/src/main/java/web/config/JobAdvertisementApplicationContext.java
@@ -1,11 +1,5 @@
 package web.config;
 
-import hu.iit.me.controller.ApplicantController;
-import hu.iit.me.controller.JobController;
-import hu.iit.me.service.ApplicantService;
-import hu.iit.me.service.JobService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -14,24 +8,5 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @ComponentScan({"web.config", "hu.iit.me.controller", "hu.iit.me.dao", "hu.iit.me.service"})
 public class JobAdvertisementApplicationContext {
-
-    private JobService jobService;
-    private ApplicantService applicantService;
-
-    @Autowired
-    public JobAdvertisementApplicationContext(JobService jobService, ApplicantService applicantService) {
-        this.jobService = jobService;
-        this.applicantService = applicantService;
-    }
-
-    @Bean
-    public JobController jobController() {
-        return new JobController(jobService);
-    }
-
-    @Bean
-    public ApplicantController applicantController() {
-        return new ApplicantController(applicantService);
-    }
 
 }
